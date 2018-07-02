41af4698090d252996e16cffc4d3b73117243230
Vlad Teplitski <vl4d89@gmail.com>
2018-07-02 18:38:52 +0300

    latest

--- src/jsonjs.js
+++ src/jsonjs.js
@@ -15,6 +15,37 @@ var daylow;
 var todaysopen;
 var previousclose;
 
+
+function getNewData() {
+
+var settings = {
+		  "async": true,
+		  "crossDomain": true,
+		  "url": "https://api.gto.co.il:9005/V2/json/login",
+		  "method": "POST",
+		  "headers": {
+		    "Content-Type": "application/json",
+		    "Cache-Control": "no-cache"
+		  },
+		  "processData": false,
+		  "data": "{\n\t\"Login\": {\n\t\t\n\t\t\"User\":\"apizvi01\",\n\t\t\"Password\":\"12345\"\n\t\n\t}\n}"
+		}
+
+
+	document.getElementById("test0011").innerHTML = "before ajax";
+	
+	
+	$.ajax(settings).done(function (response) {
+		  console.log(response);
+		});
+
+	document.getElementById("test0012").innerHTML = "Finished ajax";
+	
+	
+}
+
+
+
 function getDailyDatasets() {
     var callUrl = "https://api.stockdio.com/data/financial/prices/v1/getlatestpricesex";
     var dataParam = "app-key=F2A5C7076BE141C79350BFA00AE3EC4B&stockExchange=TASE&symbols=DEDR.L;";
